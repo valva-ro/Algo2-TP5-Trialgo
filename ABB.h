b638dc089291db6f24bbf002d7d5a5939a570da5
Valentina Varela Rodriguez <vavarela.rodriguez@gmail.com>
2020-07-18 01:07:15 -0300

    refactor: Agregar comentarios de los metodos

--- ABB.h
+++ ABB.h
@@ -7,42 +7,141 @@ using namespace std;
 template <typename Tipo>
 class ABB {
 
+    // Atributos
     private:
         NodoABB<Tipo>* raiz;
 
+    // Metodos
     public:
+
+        //  PRE: -
+        // POST: Construye un objeto de tipo ABB con la raiz apuntando a NULL
         ABB();
+
+        //  PRE: El arbol esta creado
+        // POST: Libera los recursos del ABB
         ~ABB();
+
+        //  PRE: -
+        // POST: Devuelve la raiz
         NodoABB<Tipo>* obtenerRaiz();
-        NodoABB<Tipo>* obtenerMaxNode(NodoABB<Tipo>* nodo);
-        NodoABB<Tipo>* obtenerMinNode(NodoABB<Tipo>* nodo);
+
+        //  PRE: El nodo tiene que pertenecer al ABB
+        // POST: Devuelve el valor maximo desde el nodo
+        NodoABB<Tipo>* obtenerMax(NodoABB<Tipo>* nodo);
+
+        //  PRE: El nodo tiene que pertenecer al ABB
+        // POST: Devuelve el nodo minimo desde el nodo
+        NodoABB<Tipo>* obtenerMin(NodoABB<Tipo>* nodo);
+
+        //  PRE: -
+        // POST: Devuelve la altura
         int obtenerAltura();
+
+        //  PRE: -
+        // POST: Si el dato no existe en el arbol lo agrega
         void insertar(Tipo dato);
+
+        //  PRE: -
+        // POST: Si el dato existe en el arbol lo borra
         void borrarDato(Tipo dato);
+
+        //  PRE: -
+        // POST: Libera los recursos del ABB
         void borrarTodo();
+
+        //  PRE: -
+        // POST: Devuelve true si el arbol esta balanceado, de lo contrario false
         bool estaBalanceado();
+
+        //  PRE: -
+        // POST: Devuelve true si el dato existe en el arbol, de lo contrario false
         bool existe(Tipo dato);
+
+        //  PRE: -
+        // POST: Si existe el dato, devuelve el sucesor del mismo
         Tipo sucesor(Tipo dato);
+
+        //  PRE: -
+        // POST: Si existe el dato, devuelve el predecesor del mismo
         Tipo predecesor(Tipo dato);
-        void inOrder(); // izq, padre, der
-        void preOrder(); // padre, izq, der
-        void postOrder(); // izq, der, padre
+
+        //  PRE: El arbol debe tener al menos un dato
+        // POST: Muestra por pantalla el recorrido in order (izquierda, padre, derecha)
+        void inOrder();
+
+        //  PRE: El arbol debe tener al menos un dato
+        // POST: Muestra por pantalla el recorrido pre order (padre, izquierda, derecha)
+        void preOrder();
+
+        //  PRE: El arbol debe tener al menos un dato
+        // POST: Muestra por pantalla el recorrido post order (izquierda, derecha, padre)
+        void postOrder();
+
+        //  PRE: -
+        // POST: Muestra info basica del ABB
         void mostrarData();
 
     private:
+
+        //  PRE: El dato no debe estar en el arbol, y ademas tiene que ser el dato del nodo
+        // POST: Inserta el dato en el arbol
         NodoABB<Tipo>* insertar(NodoABB<Tipo>* nodo, Tipo dato);
+
+        //  PRE: -
+        // POST: Devuelve true si el nodo esta balanceado, de lo contrario false
         bool estaBalanceado(NodoABB<Tipo>* nodo);
+
+        //  PRE: -
+        // POST: Devuelve la altura del nodo
         int obtenerAltura(NodoABB<Tipo>*nodo);
+
+        //  PRE: El dato debe ser el dato del nodo
+        // POST: Si el dato existe en el arbol, lo elimina y devuelve el nodo que lo reemplazaria.
+        //       De lo contrario devuelve el nodo recibido
         NodoABB<Tipo>* borrarDato(NodoABB<Tipo>* nodo, Tipo dato);
+
+        //  PRE: El nodo debe ser hoja
+        // POST: Si el dato existe en el arbol, lo elimina y devuelve el nodo que lo reemplazaria.
+        //       De lo contrario devuelve el nodo recibido
         NodoABB<Tipo>* borradoHoja(NodoABB<Tipo>* nodo);
+
+        //  PRE: El nodo debe tener solo un hijo
+        // POST: Si el dato existe en el arbol, lo elimina y devuelve el nodo que lo reemplazaria.
+        //       De lo contrario devuelve el nodo recibido
         NodoABB<Tipo>* borradoUnHijo(NodoABB<Tipo>* nodo);
+
+        //  PRE: El nodo debe tener ambos hijos
+        // POST: Si el dato existe en el arbol, lo elimina y devuelve el nodo que lo reemplazaria.
+        //       De lo contrario devuelve el nodo recibido
         NodoABB<Tipo>* borradoDosHijos(NodoABB<Tipo>* nodo);
+
+        //  PRE: -
+        // POST: Libera los recursos utilizados por el ABB
         void borrarTodo(NodoABB<Tipo>* nodo);
+
+        //  PRE: El dato debe ser el dato del nodo
+        // POST: Devuelve true si el dato existe en el ABB, de lo contrario false
         NodoABB<Tipo>* existe(NodoABB<Tipo>* nodo, Tipo dato);
+
+        //  PRE: El nodo debe existir en el ABB
+        // POST: Devuelve el nodo sucesor de nodo
         Tipo sucesor(NodoABB<Tipo>* nodo);
+
+        //  PRE: El nodo debe existir en el ABB
+        // POST: Devuelve el nodo predecesor de nodo
         Tipo predecesor(NodoABB<Tipo>* nodo);
+
+        //  PRE: El nodo debe pertenecer al arbol
+        // POST: Muestra por pantalla el recorrido in order
         void inOrder(NodoABB<Tipo>* nodo);
+
+        //  PRE: El nodo debe pertenecer al arbol
+        // POST: Muestra por pantalla el recorrido pre order
         void preOrder(NodoABB<Tipo>* nodo);
+
+        //  PRE: El nodo debe pertenecer al arbol
+        // POST: Muestra por pantalla el recorrido post order
         void postOrder(NodoABB<Tipo>* nodo);
 };
 
@@ -63,7 +162,10 @@ ABB<Tipo>:: ~ABB() {
 ////////////////////////////////////////////////
 template <typename Tipo>
 void ABB<Tipo>:: insertar(Tipo dato) {
-    raiz = insertar(raiz, dato);
+    if (!existe(dato))
+        raiz = insertar(raiz, dato);
+    else
+        cout << "\tEl dato " << dato << " ya existe en el arbol!\n";
 }
 
 ////////////////////////////////////////////////
@@ -80,7 +182,7 @@ NodoABB<Tipo>* ABB<Tipo>:: obtenerRaiz() {
 
 ////////////////////////////////////////////////
 template <typename Tipo>
-NodoABB<Tipo>* ABB<Tipo>:: obtenerMaxNode(NodoABB<Tipo>* nodo) {
+NodoABB<Tipo>* ABB<Tipo>:: obtenerMax(NodoABB<Tipo>* nodo) {
     if (!raiz) {
         cout <<  "El ABB esta vacio!" << endl;
         return 0;
@@ -92,22 +194,23 @@ NodoABB<Tipo>* ABB<Tipo>:: obtenerMaxNode(NodoABB<Tipo>* nodo) {
 
 ////////////////////////////////////////////////
 template <typename Tipo>
-NodoABB<Tipo>* ABB<Tipo>:: obtenerMinNode(NodoABB<Tipo>* nodo) {
+NodoABB<Tipo>* ABB<Tipo>:: obtenerMin(NodoABB<Tipo>* nodo) {
     if (!raiz) {
         cout <<  "El ABB esta vacio!" << endl;
         return 0;
     }
     while(nodo->obtenerIzquierda())
         nodo = nodo->obtenerIzquierda();
-
     return nodo;
 }
 
 ////////////////////////////////////////////////
 template <typename Tipo>
 void ABB<Tipo>:: borrarDato(Tipo dato) {
-    cout << "Deleting dato: " << dato << "\n";
-    raiz = borrarDato(raiz, dato);
+    if (existe(dato))
+        raiz = borrarDato(raiz, dato);
+    else
+        cout << "\tEl dato " << dato << " no existe!\n";
 }
 
 ////////////////////////////////////////////////
@@ -132,26 +235,19 @@ bool ABB<Tipo>:: existe(Tipo dato) {
 ////////////////////////////////////////////////
 template <typename Tipo>
 Tipo ABB<Tipo>::sucesor(Tipo dato) {
-
-    NodoABB<Tipo>* datoNode = existe(raiz, dato);
-
-    if(datoNode == 0)
-        return -1;
-    else
-        return sucesor(datoNode);
+    if (existe(dato)) {
+        NodoABB<Tipo> *datoNodo = existe(raiz, dato);
+        return sucesor(datoNodo);
+    }
 }
 
 ////////////////////////////////////////////////
 template <typename Tipo>
 Tipo ABB<Tipo>::predecesor(Tipo dato) {
-
-    NodoABB<Tipo>* datoNode = existe(raiz, dato);
-
-    if(datoNode == 0)
-        return -1;
-    else
-        return predecesor(datoNode);
-
+    if (existe(dato)) {
+        NodoABB<Tipo> *datoNodo = existe(raiz, dato);
+        return predecesor(datoNodo);
+    }
 }
 
 ////////////////////////////////////////////////
@@ -189,8 +285,8 @@ void ABB<Tipo>:: mostrarData() {
 
     cout << "\n\tLa raiz del ABB es: " << raiz->obtenerDato() << "\n";
     cout << "\tLa altura del ABB es: " << obtenerAltura() << "\n";
-    cout << "\tMax: " << obtenerMaxNode(raiz)->obtenerDato() << "\n";
-    cout << "\tMin: " << obtenerMinNode(raiz)->obtenerDato() << "\n";
+    cout << "\tMax: " << obtenerMaxNodo(raiz)->obtenerDato() << "\n";
+    cout << "\tMin: " << obtenerMinNodo(raiz)->obtenerDato() << "\n";
 
     if (estaBalanceado())
         cout << "\tEl ABB esta balanceado! " << "\n\n";
@@ -225,7 +321,6 @@ template<typename Tipo>
 int ABB<Tipo>:: obtenerAltura(NodoABB<Tipo>* nodo) {
     if (!nodo)
         return 0;
-
     return 1 + max(obtenerAltura(nodo->obtenerIzquierda()), obtenerAltura(nodo->obtenerDerecha()));
 }
 
@@ -234,7 +329,6 @@ template<typename Tipo>
 bool ABB<Tipo>:: estaBalanceado(NodoABB<Tipo>* nodo) {
     if (!nodo)
         return false;
-
     return abs(obtenerAltura(nodo->obtenerIzquierda()) - obtenerAltura(nodo->obtenerDerecha())) <= 1;
 }
 
@@ -249,29 +343,29 @@ NodoABB<Tipo>* ABB<Tipo>:: borradoHoja(NodoABB<Tipo>* nodo) {
 template<typename Tipo>
 NodoABB<Tipo>* ABB<Tipo>:: borradoUnHijo(NodoABB<Tipo>* nodo) {
 
-    if (nodo->onlyRightChildren()) {
-        NodoABB<Tipo>* rightNode = nodo->obtenerDerecha();
-        rightNode->setParent(nodo->obtenerParent());
+    if (nodo->soloHijoDerecha()) {
+        NodoABB<Tipo>* nodoDer = nodo->obtenerDerecha();
+        nodoDer->asignarPadre(nodo->obtenerParent());
         NodoABB<Tipo>* aux = nodo;
         delete aux;
-        return rightNode;
+        return nodoDer;
     }
 
-    else if (nodo->onlyLeftChildren()) {
-        NodoABB<Tipo>* leftNode = nodo->obtenerIzquierda();
-        leftNode->setParent(nodo->obtenerParent());
+    else if (nodo->soloHijoIzquierda()) {
+        NodoABB<Tipo>* nodoIzq = nodo->obtenerIzquierda();
+        nodoIzq->asignarPadre(nodo->obtenerParent());
         NodoABB<Tipo>* aux = nodo;
         delete aux;
-        return leftNode;
+        return nodoIzq;
     }
 }
 
 ////////////////////////////////////////////////
 template<typename Tipo>
 NodoABB<Tipo>* ABB<Tipo>:: borradoDosHijos(NodoABB<Tipo>* nodo) {
-    Tipo sucesordato = sucesor(nodo->obtenerDato());
-    nodo->setdato(sucesordato);
-    nodo->asignarDerecha(borrarDato(nodo->obtenerDerecha(), sucesordato));
+    Tipo sucesorDato = sucesor(nodo->obtenerDato());
+    nodo->asignarDato(sucesorDato);
+    nodo->asignarDerecha(borrarDato(nodo->obtenerDerecha(), sucesorDato));
     return nodo;
 }
 
@@ -279,27 +373,24 @@ NodoABB<Tipo>* ABB<Tipo>:: borradoDosHijos(NodoABB<Tipo>* nodo) {
 template<typename Tipo>
 NodoABB<Tipo>* ABB<Tipo>:: borrarDato(NodoABB<Tipo>* nodo, Tipo dato) {
 
-    if (!nodo)
-        return nodo;
+    if (existe(nodo)) {
+        if (nodo->obtenerDato() == dato) {
 
-    if(nodo->obtenerDato() == dato) {
+            if (nodo->esHoja())
+                nodo = borradoHoja(nodo);
 
-        if (nodo->esHoja())
-            nodo = borradoHoja(nodo);
+            else if (nodo->soloHijoDerecha() || nodo->soloHijoIzquierda())
+                nodo = borradoUnHijo(nodo);
 
-        else if (nodo->onlyRightChildren() || nodo->onlyLeftChildren())
-            nodo = borradoUnHijo(nodo);
+            else
+                nodo = borradoDosHijos(nodo);
+        }
+        else if (nodo->obtenerDato() < dato)
+            nodo->asignarDerecha(borrarDato(nodo->obtenerDerecha(), dato));
 
         else
-            nodo = borradoDosHijos(nodo);
+            nodo->asignarIzquierda(borrarDato(nodo->obtenerIzquierda(), dato));
     }
-
-    else if (nodo->obtenerDato() < dato)
-        nodo->asignarDerecha(borrarDato(nodo->obtenerDerecha(), dato));
-
-    else
-        nodo->asignarIzquierda(borrarDato(nodo->obtenerIzquierda(), dato));
-
     return nodo;
 }
 
@@ -327,9 +418,9 @@ NodoABB<Tipo>* ABB<Tipo>:: existe(NodoABB<Tipo> *nodo, Tipo dato) {
 template<typename Tipo>
 Tipo ABB<Tipo>:: sucesor(NodoABB<Tipo>* nodo) {
 
-    if (!nodo->onlyRightChildren()) {
-        NodoABB<Tipo>* minNode = obtenerMinNode(nodo->obtenerDerecha());
-        return minNode->obtenerDato();
+    if (!nodo->soloHijoDerecha()) {
+        NodoABB<Tipo>* minNodo = obtenerMinNodo(nodo->obtenerDerecha());
+        return minNodo->obtenerDato();
     }
 
     NodoABB<Tipo>* sucesor = 0;
@@ -349,8 +440,8 @@ Tipo ABB<Tipo>:: sucesor(NodoABB<Tipo>* nodo) {
 ////////////////////////////////////////////////
 template<typename Tipo>
 Tipo ABB<Tipo>:: predecesor(NodoABB<Tipo>* nodo) {
-    if (!nodo->onlyLeftChildren())
-        return obtenerMinNode(nodo->onlyLeftChildren());
+    if (!nodo->soloHijoIzquierda())
+        return obtenerMinNodo(nodo->soloHijoIzquierda());
 
     NodoABB<Tipo>* sucesor = 0;
     NodoABB<Tipo>* predecesor = raiz;

462cdd5b96eeef4a63f68fbe4190ab3e0b0a78a1
root-MKevin <kmeaca@fi.uba.ar>
2020-07-31 18:55:22 -0300

    Arregle el ultimo commit.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
-
+/*
+  75.41/95.15: Algoritmos y Programacion II - Catedra Juarez - 1°C 2020
+  Trabajo Practico Grupal N5
+  Hecho por
+            Alejo Villores
+            Alex Teper
+            Kevin Meaca
+            Valentina Varela Rodriguez
+*/
+#include "Menu.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Menu menu;
+
+    menu.mostrarOpciones();
     return 0;
 }

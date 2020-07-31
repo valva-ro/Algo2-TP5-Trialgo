598c0939855c7f05fb615dd1d7ea99bc5d00d82b
Valentina Varela <vavarela.rodriguez@gmail.com>
2020-07-31 00:48:18 -0300

    refactor: Agregue comentario explicando como funcionan los argumentos

--- main.cpp
+++ main.cpp
@@ -6,13 +6,18 @@
             Alex Teper
             Kevin Meaca
             Valentina Varela Rodriguez
+
+  Si al ejecutar por consola se recibe:
+    - Un argumento ----> los nombres de los archivos seran "archivoAeropuertos.txt" y
+                         "archivoVuelos.txt"
+    - Dos argumentos --> los nombres de los archivos seran el segundo argumento para
+                         los aeropuertos y "archivoVuelos.txt"
+    - Tres argumentos -> los nombres de los archivos seran el segundo argumento para
+                         los aeropuertos y el tercer argumento para los vuelos
 */
 #include "Menu.h"
-const int EJECUTABLE = 1;
 const int AEROPUERTOS_TXT = 2;
 const int AEROPUERTO_Y_VUELOS_TXT = 3;
-
-
 
 int main(int argc, char *argv[]) {
 
@@ -20,12 +25,16 @@ int main(int argc, char *argv[]) {
     string archVuelos = "archivoVuelos.txt";
 
     switch (argc) {
-        case AEROPUERTOS_TXT: archAeropuertos= argv[1]; break;
-        case AEROPUERTO_Y_VUELOS_TXT: archAeropuertos= argv[1]; archVuelos=argv[2]; break;
+        case AEROPUERTOS_TXT:
+            archAeropuertos = argv[1];
+            break;
+        case AEROPUERTO_Y_VUELOS_TXT:
+            archAeropuertos = argv[1];
+            archVuelos = argv[2];
+            break;
     }
 
     Menu(archAeropuertos,archVuelos);
-
 
     return 0;
 }

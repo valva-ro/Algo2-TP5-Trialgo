26a668a4173369f350159958de04c28a7097fee8
AlexTeper99 <67521917+AlexTeper99@users.noreply.github.com>
2020-08-02 23:09:53 -0300

    Creacion de la clase grafo y sus principales metodos (faltan los recorridos minimos)

---
+++ Grafo.cpp
@@ -0,0 +1,37 @@
+//
+// Created by alexteper on 2/8/20.
+//
+
+#include "Grafo.h"
+
+Grafo::Grafo() {
+
+}
+
+Grafo::Grafo(Matriz<int> *precio, Matriz<float> *distancia, int elementos, Lista<string> *vertices) {
+
+}
+
+Grafo::~Grafo() {
+
+}
+
+bool Grafo::existeArista(string origen, string destino) {
+
+}
+
+bool Grafo::existeVertice(string vertice) {
+
+}
+
+int Grafo::obtenerDistancia(string origen, string destino) {
+
+}
+
+int Grafo::obtenerPrecio(string origen, string destino) {
+
+}
+
+void Grafo::insertarArista(string origen, string destino, int precio, float distancia) {
+
+}

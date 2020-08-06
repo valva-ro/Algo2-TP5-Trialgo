14ab9d50c72ea5ab19373f89f7ecc88b136d1beb
Valentina Varela <vavarela.rodriguez@gmail.com>
2020-08-06 17:03:18 -0300

    fix

--- Cargador.cpp
+++ Cargador.cpp
@@ -22,7 +22,7 @@ void Cargador::cargarDatos(Diccionario<string, Aeropuerto*> *&arbolAeropuertos, 
     archivo.close();
 }
 
-void Cargador::cargarDatos(Grafo *&grafoVuelos, string ruta)
+void Cargador::cargarDatos(Grafo *&grafoVuelos, const string& ruta)
 {
     ifstream archivo;
     archivo.open(ruta.c_str(),ios::in);
